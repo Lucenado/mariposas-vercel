1b29d7917412551cee8e526f955332d4ad466e25
Pedro <pedrolucenado@gmail.com>
2023-10-16 15:08:43 -0300

    reversing db to elephant

--- server/databasepg.js
+++ server/databasepg.js
@@ -2,17 +2,17 @@ const {Pool} = require('pg');
 
 require('dotenv').config();
 
-/*const pool = new Pool({
-    connectionString: process.env.CONNECTION_STRING,
-})*/
-
 const pool = new Pool({
+    connectionString: process.env.CONNECTION_STRING,
+})
+
+/*const pool = new Pool({
     user: "postgres",
     host: "localhost",
     database: "fischercopy",
     password: "postgres",
     port: 5432,
-});
+});*/
 
 /*pool.connect((err) => {
     if (err) {throw err}
