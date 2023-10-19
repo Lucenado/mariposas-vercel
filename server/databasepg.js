e862385d5646afc8b067d2b64b299913a4d8c076
Pedro <pedrolucenado@gmail.com>
2023-10-19 12:41:50 -0300

    max to 3

--- server/databasepg.js
+++ server/databasepg.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.CONNECTION_STRING,
-    max: 5,
+    max: 3,
     min: 2,
     idleTimeoutMillis: 1000 //close idle clients after 1 second
 });
